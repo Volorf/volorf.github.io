1967e91a8941b03bbfe04a3ff30f3a18f9199c98
Oleg <frolololeg@gmail.com>
2019-11-19 21:49:58 +0300

    Fixed links with a HashRouter component

---
+++ js/particles_app.js
@@ -0,0 +1,5 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+// alert("but it works");
+particlesJS.load('particles_js', 'assets/particles.json', function() {
+    // alert("it works");
+  });

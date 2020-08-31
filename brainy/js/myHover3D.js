b92fb82c2a2a3f2ccb0d6e0d8fc8ebc17132e19e
Oleg Frolov <frolololeg@gmail.com>
2020-08-31 20:29:50 +0300

    Fixed redirect.js

--- brainy/js/myHover3D.js
+++ brainy/js/myHover3D.js
@@ -1,3 +1,7 @@
 let hoverEffectCard = new Hover3D(".screenshot");
 hoverEffectCard.xOffset = 10;
 hoverEffectCard.yOfffset = 30;
+
+let hoverEffectStoreButtons = new Hover3D(".storeButton");
+hoverEffectStoreButtons.xOffset = 5;
+hoverEffectStoreButtons.yOffset = 15;
--- brainy/js/redirect.js
+++ brainy/js/redirect.js
@@ -1,28 +1,34 @@
 const noneMobileLink = "http://olegfrolov.design/brainy/";
-const iosStoreLink = "";
-const androidStoreLink = "";
+const iosStoreLink = "https://apps.apple.com/ru/app/brainy-challenge-you-brain/id1529786675";
+const androidStoreLink = "https://play.google.com/store/apps/details?id=com.Froly.Brainy";
 
 
-function getMobileOperatingSystem() {
-    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+function getMobileOperatingSystem() 
+{
+    let userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
     // Windows Phone must come first because its UA also contains "Android"
-    if (/windows phone/i.test(userAgent)) {
+    if (/windows phone/i.test(userAgent)) 
+    {
         return "Windows Phone";
     }
 
-    if (/android/i.test(userAgent)) {
+    if (/android/i.test(userAgent)) 
+    {
         return "Android";
     }
 
     // iOS detection from: http://stackoverflow.com/a/9039885/177710
-    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) 
+    {
         return "iOS";
     }
 
     return "unknown";
 }
-function DetectAndServe() {
+
+function DetectAndServe() 
+{
     if (getMobileOperatingSystem() == "Android") {
         window.location.href = androidStoreLink;
     }
@@ -35,4 +41,4 @@ function DetectAndServe() {
     // if (getMobileOperatingSystem() == "unknown") {
     //     window.location.href = noneMobileLink;
     // }
-};
+}
